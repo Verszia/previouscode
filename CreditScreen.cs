3a12b34d6bdb7d3e722c6b7ecffd9f18cc6135a6
Zachary Combs <50453833+Verszia@users.noreply.github.com>
2021-05-31 11:45:02 -0400

    Add files via upload

---
+++ CreditScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScreen : MonoBehaviour
+{
+    public float sec = 17f;
+    public GameObject returnButton;
+    public GameObject quitButton;
+
+    void Start()
+    {
+        if (returnButton.activeInHierarchy)
+        {
+            returnButton.SetActive(false);
+        }
+
+        if (quitButton.activeInHierarchy)
+        {
+            quitButton.SetActive(false);
+        }
+
+        StartCoroutine(ActivationRoutine());
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        yield return new WaitForSeconds(sec);
+
+        returnButton.SetActive(true);
+        quitButton.SetActive(true);
+
+    }
+
+}
